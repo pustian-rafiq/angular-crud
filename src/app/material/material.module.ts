e6f5e388f9a2cca34e4f424e66565adab0e05f2a
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2023-01-25 22:55:39 +0600

    Design add product form

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const MaterialComponentModule = [
@@ -11,6 +17,13 @@ const MaterialComponentModule = [
   MatFormFieldModule,
   MatToolbarModule,
   MatIconModule,
+  MatInputModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatRadioModule,
+  ReactiveFormsModule,
+  FormsModule,
 ];
 @NgModule({
   imports: [MaterialComponentModule],
--- src/app/product-modal/product-modal.component.ts
+++ src/app/product-modal/product-modal.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-modal',
   templateUrl: './product-modal.component.html',
-  styleUrls: ['./product-modal.component.css']
+  styleUrls: ['./product-modal.component.css'],
 })
 export class ProductModalComponent {
+  freshness: string[] = ['Brand New', 'Second Hand', 'Fabrished Product'];
+  productForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category: ['', Validators.required],
+      price: ['', Validators.required],
+      freshness: ['', Validators.required],
+      date: ['', Validators.required],
+    });
+  }
+
+  addProduct(): void {
+    console.log(this.productForm.value);
+  }
 }
